f34389c778138d4a596d901b11d94dc866921468
iamclement1 <blessedmarcel1@gmail.com>
2021-03-18 17:50:35 +0100

    validation

--- js/validate.js
+++ js/validate.js
@@ -1,9 +1,10 @@
-$('.toggle-password').click(function () {
-  $(this).toggleClass('fa-eye fa-eye-slash');
-  var input = $($(this).attr('toggle'));
-  if (input.attr('type') == 'password') {
-    input.attr('type', 'text');
-  } else {
-    input.attr('type', 'password');
-  }
-});
+function validate() {
+  var validationField = document.getElementById('validation-txt');
+  var password = document.getElementById('password-2');
+
+  var content = password.value;
+  var errors = [];
+}
+if (content.length < 8) {
+  errors.push('Your password must be at least 8 characters');
+}
